124039fab3441b8440a26ce48d6ad4644c7d01fa
hemantgrovo <36243445+hemantgrovo@users.noreply.github.com>
2018-04-02 13:57:51 -0700

    Composer Installation (#5)

--- grovo.sh
+++ grovo.sh
@@ -1,5 +1,5 @@
 # install software using cask specific to Grovo
-brew cask install -y composer phpstorm phpini
+brew cask install -y phpstorm phpini
 
 #install liquibase
 brew install liquibase
@@ -12,4 +12,10 @@ brew install homebrew/php/php71-mcrypt
 
 brew install homebrew/php/php71 --with-postgresql --with-phpdbg
 #Please install xcode before this as xdebug requires xcode and xdebug is used by codecov
+brew install git-flow php7x-xdebug yarn
+
+#Install composer package to install services dependencies
+brew install composer
+
 brew install git-flow homebrew/php/php71-xdebug yarn
+
