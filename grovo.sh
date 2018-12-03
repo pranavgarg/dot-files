db22301abc9d32ea5a01f225d8122374f85f2abd
victoriawugrovo <37307264+victoriawugrovo@users.noreply.github.com>
2018-12-03 15:47:30 -0800

    Move scala sbt install after installing java (#8)
    * install env for scala projects
    
    * remove brew install scala sbt before java

--- grovo.sh
+++ grovo.sh
@@ -1,4 +1,19 @@
-#!/bin/bash
+
+#install liquibase
+brew install liquibase
+
+#install glide for golang package
+brew install glide
+
+brew install scala sbt
+
+#homebrew/php xdebug is deprecated, install it this way
+pecl install xdebug
+
+brew install docker-compose
+
+#Install composer package to install services dependencies
+brew install composer
 
 docker login
 
